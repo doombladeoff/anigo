0d44f78c66bf3f615617213c57506e2f5a3e7c0a
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2025-05-24 12:17:41 +0300

    Update getRecommendations.ts

--- api/Yummy/getRecommendations.ts
+++ api/Yummy/getRecommendations.ts
@@ -3,13 +3,12 @@ import UserAgents from "user-agents";
 
 export const getRecommendations = async () => {
     try {
-        const response = await axios.get(`https://api.yani.tv/anime?min_age=4&status=released&status=ongoing&types=tv&types=movie&min_rating=8&ep_from=1&sort_forward=true&sort=random&offset=${Math.floor(Math.random() * 11)}&limit=5`,
-        {
-            headers: {
-                "User-Agent": new UserAgents().toString(),
-            },
-        });
-
+        const response = await axios.get(`https://api.yani.tv/anime?min_age=3&status=released&status=ongoing&types=tv&min_rating=8&ep_from=1&sort_forward=true&sort=random&limit=5`,
+            {
+                headers: {
+                    "User-Agent": new UserAgents().toString(),
+                },
+            });
         return response.data.response;
 
     } catch (e) {
