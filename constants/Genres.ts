4457702106e5fb9654c54683ea85644a8d490f0a
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2025-06-06 18:02:32 +0300

    Create Genres.ts

---
+++ constants/Genres.ts
@@ -0,0 +1,18 @@
+export const genresMap = {
+    1: { en: "Action", ru: "Экшен" },
+    2: { en: "Adventure", ru: "Приключения" },
+    4: { en: "Comedy", ru: "Комедия" },
+    5: { en: "Avant-Garde", ru: "Авангард" },
+    7: { en: "Mystery", ru: "Детектив" },
+    8: { en: "Drama", ru: "Драма" },
+    9: { en: "Ecchi", ru: "Этти" },
+    10: { en: "Fantasy", ru: "Фэнтези" },
+    14: { en: "Horror", ru: "Ужасы" },
+    22: { en: "Romance", ru: "Романтика" },
+    24: { en: "Sci-Fi", ru: "Научная фантастика" },
+    30: { en: "Sports", ru: "Спорт" },
+    36: { en: "Slice-of-Life", ru: "Повседневность" },
+    37: { en: "Supernatural", ru: "Сверхъестественное" },
+    117: { en: "Suspense", ru: "Саспенс" },
+    543: { en: "Gourmet", ru: "Гурман" }
+};
